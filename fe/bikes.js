b286761ddba37e5e7e6c357e9b23b5b9d0a3a7f0
Tom <106186592+TomiSach@users.noreply.github.com>
2023-05-15 23:22:58 -0300

    Add files via upload

---
+++ fe/bikes.js
@@ -0,0 +1,29 @@
+// Función para mostrar las motos
+function displayBikes(bikeList) {
+    let productsHTML = '';
+    bikeList.forEach(element => {
+      productsHTML += `
+        <div class="item-container">
+          <h3>${element.brand}</h3>
+          <h4>${element.model}</h4>
+          <img src="${element.photo}" alt="Vehicle Photo">
+          <h4 id="car-price1" style="color:goldenrod">${element.price} u$d</h4>
+          <p><i>${element.fuel}<br>${element.kms} kms <br> It's a ${element.year} model</i></p>
+          <a href="mailto:${element.email}"><button class="button-ask">Ask</button></a>
+        </div>
+      `;
+    });
+    document.getElementById('page-content-bikes').innerHTML = productsHTML;
+  }
+  
+  // Carga los datos de las motos y muestra la lista
+  window.onload = async () => {
+    try {
+      const response = await fetch("/api/motoProducto");
+      const bikeList = await response.json();
+      console.log(bikeList);
+      displayBikes(bikeList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
---
+++ fe/cars.js
@@ -0,0 +1,30 @@
+// Función para mostrar los autos
+function displayCars(carList) {
+  let productsHTML = '';
+  carList.forEach(element => {
+    productsHTML += `
+    <div class="item-container">
+      <h3>${element.brand}</h3>
+      <h4>${element.model}</h4>
+      <img src="${element.photo}" alt="Vehicle Photo">
+      <h4 id="car-price1" style="color:goldenrod">${element.price} u$d</h4>
+      <h4>${element.model}</h4>
+      <p><i>${element.fuel}<br>${element.kms} kms <br> It's a ${element.year} model</i></p>
+      <a href="mailto:${element.email}"><button class="button-ask">Ask</button></a>
+    </div>
+  `;
+  });
+  document.getElementById('page-content-cars').innerHTML = productsHTML;
+}
+
+// Carga los datos de los autos y muestra la lista
+window.onload = async () => {
+  try {
+    const response = await fetch("/api/autoProducto");
+    const carList = await response.json();
+    console.log(carList);
+    displayCars(carList);
+  } catch (error) {
+    console.error(error);
+  }
+};
